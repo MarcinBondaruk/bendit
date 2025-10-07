150ce12500c2831738224c6a53cd4a2c7ddb82e9
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2025-10-07 22:30:56 +0200

    feat: run treafik with docker compose

--- services/run_service.sh
+++ services/run_service.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export TRAEFIK_CONF_PATH="/home/bendit/services"
+export BENDIT_UID=$( getent passwd docker | cut -d: -f3 )
+export BENDIT_GID=$( getent group docker | cut -d: -f3 )
+
+docker network create proxy
 docker compose pull
-docker compose up
+docker compose up -d
 
 exit 0
